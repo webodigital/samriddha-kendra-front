db11990a28937a637d5a822b18013a9a55b1e395
mohitanand744 <jhamohit744@gmail.com>
2024-10-01 18:37:19 +0530

    working on Privacy Page

--- src/Pages/Privacy_policy.jsx
+++ src/Pages/Privacy_policy.jsx
@@ -2,9 +2,142 @@ import React from "react";
 
 const Privacy_policy = () => {
   return (
-    <center className="mt-15 h-100">
-      <h1 className="fw-bolder font-inter">Privacy policy Page</h1>
-    </center>
+    <div className="privacy font-inter">
+      <center className="mt-7 bg-dark-blue text-white p-5">
+        <h1 className="fw-bolder font-inter">Privacy Policy</h1>
+      </center>
+      <div className="container fs-18 my-5 px-5">
+        <p>
+          This Privacy Notice for Samriddh Kendra operated by Creditbucket
+          Technologies Private Limited (we', 'us', or 'our'), describes how and
+          why we might access, collect, store, use, and/or share (process') your
+          personal information when you use our services (Services'), including
+          when you:
+        </p>
+
+        <ul className="my-5">
+          <li>
+            Visit our website at https://www.samriddhkendra.in, or any website
+            of ours that links to this Privacy Notice
+          </li>
+          <li>
+            Use Samriddh Kendra - to connect rural customer with reliable
+            services. We at SAMRIDDH KENDRA are committed to protecting the
+            privacy and security of its users (“you”, “your”, “user(s)”, or
+            “subscriber(s)”). Your privacy is important and maintaining your
+            trust is paramount to us. This Privacy Policy (“Privacy Policy”)
+            explains how we collect, use, process, disclose, and safeguard your
+            information when you access and use our Services through our website
+            and mobile app. respectively (collectively referred to as the
+            “Platform”) located at: https://www.Samriddhkendra.in/ which shall
+            be incorporated into and subject to the Terms of Service of the
+            Platform and shall be read harmoniously and in conjunction.
+          </li>
+          <li>
+            Engage with us in other related ways, including any sales,
+            marketing, or events
+          </li>
+        </ul>
+
+        <p className="my-5">
+          Questions or concerns? Reading this Privacy Notice will help you
+          understand your privacy rights and choices. We are responsible for
+          making decisions about how your personal information is processed. If
+          you do not agree with our policies and practices, please do not use
+          our Services. If you still have any questions or concerns, please
+          contact us at{" "}
+          <span className="fw-bold text-info px-2">
+            Support@samriddhkendra.in
+          </span>
+          This Privacy Policy outlines how we collect, use, and protect your
+          personal information in compliance with applicable laws and
+          regulations, including the Information Technology Act, 2000, the
+          Information Technology (Reasonable Security Practices and Procedures
+          and Sensitive Personal Data or Information) Rules, 2011, and the
+          Consumer Protection Act, 2019.
+        </p>
+
+        <h2 className="mt-5">SUMMARY OF KEY POINTS</h2>
+        <p>
+          This summary provides key points from our Privacy Notice, but you can
+          find out more details about any of these topics by clicking the link
+          following each key point or by using our table of contents below to
+          find the section you are looking for.
+        </p>
+
+        <h2 className="mt-5">What personal information do we process?</h2>
+        <p>
+          When you visit, use, or navigate our Services, we may process personal
+          information depending on how you interact with us and the Services,
+          the choices you make, and the products and features you use. Lear more
+          about personal information you disclose to us.
+        </p>
+        <h2 className="mt-5">
+          Do we process any sensitive personal information?
+        </h2>
+        <p>
+          databases, marketing partners, social media platforms, and other
+          outside sources. Lear more about information collected from other
+          sources.
+        </p>
+
+        <h2 className="mt-5">How do we process your information?</h2>
+        <p>
+          We process your information to provide, improve, and administer our
+          Services, communicate with you, for security and fraud prevention, and
+          to comply with law. We may also process your information for other
+          purposes with your consent. We process your information only when we
+          have a valid legal reason to do so. Learn more about how we process
+          your information.
+        </p>
+
+        <h2 className="mt-5">
+          In what situations and with which parties do we share personal
+          information?
+        </h2>
+        <p>
+          We may share information in specific situations and with specific
+          third parties. Learn more about when and with whom we share your
+          personal information.
+        </p>
+
+        <h2 className="mt-5">How do we keep your information safe?</h2>
+        <p>
+          We have adequate organisational and technical processes and procedures
+          in place to protect your personal information. However, no electronic
+          transmission over the internet or information storage technology can
+          be guaranteed to be 100% secure, so we cannot promise or guarantee
+          that hackers, cybercriminals, or other unauthorised third parties will
+          not be able to defeat our security and improperly collect, access,
+          steal, or modify your information. Learn more about how we keep your
+          information safe.
+        </p>
+
+        <h2 className="mt-5">What are your rights?</h2>
+        <p>
+          We have adequate organisational and technical processes and procedures
+          in place to protect your personal information. However, no electronic
+          transmission over the internet or information storage technology can
+          be guaranteed to be 100% secure, so we cannot promise or guarantee
+          that hackers, cybercriminals, or other unauthorised third parties will
+          not be able to defeat our security and improperly collect, access,
+          steal, or modify your information. Learn more about how we keep your
+          information safe.
+        </p>
+        <h2 className="mt-5">How do you exercise your rights?</h2>
+        <p>
+          The easiest way to exercise your rights is by submitting a data
+          subject access request, or by contacting us. We will consider and act
+          upon any request in accordance with applicable data protection laws.
+          Want to learn more about what we do with any information we collect?
+          Review the Privacy Notice in full.
+        </p>
+
+        <p className="text-warning fw-bold mt-5 title-font fs-24">
+          Still Working on Contents...!!
+        </p>
+      </div>
+    </div>
   );
 };
 
