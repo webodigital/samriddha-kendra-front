9f8227b07916b095eb24f119da35616455da901c
mohitanand744 <jhamohit744@gmail.com>
2024-10-09 16:36:06 +0530

    updated GrievanceOfficer details

--- src/Pages/GrievanceOfficer.jsx
+++ src/Pages/GrievanceOfficer.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 
 const GrievanceOfficer = () => {
+  const contactInfo = {
+    email: "grievance.samriddh@gmail.com",
+    mobile: "9153985956",
+  };
+
   return (
-    <center className="mt-15 h-100">
-      <h1 className="fw-bolder font-inter">Grievance Officer Page</h1>
-    </center>
+    <div className="container GrievanceOfficer mt-15 font-inter fs-20">
+      <h1 className="text-center mb-5 fw-bold">
+        Grievance Officer Contact Information
+      </h1>
+      <table className="table table-bordered table-striped">
+        <thead className="thead-dark">
+          <tr>
+            <th>Field</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Email ID</td>
+            <td>{contactInfo.email}</td>
+          </tr>
+          <tr>
+            <td>Mobile Number</td>
+            <td>{contactInfo.mobile}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 };
 
