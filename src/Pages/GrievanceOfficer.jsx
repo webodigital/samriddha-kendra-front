181a253ec1ad2bbc11070b7de99a9eb524b6ceb5
mohitanand744 <jhamohit744@gmail.com>
2024-10-14 15:38:43 +0530

    updated GrievanceOfficer details

--- src/Pages/GrievanceOfficer.jsx
+++ src/Pages/GrievanceOfficer.jsx
@@ -2,33 +2,53 @@ import React from "react";
 
 const GrievanceOfficer = () => {
   const contactInfo = {
-    email: "grievance.samriddh@gmail.com",
-    mobile: "9153985956",
+    merchantsupport: {
+      email: "merchantsupport@samriddhkendra.in",
+      mobile: "9153985958",
+    },
+    support: {
+      email: "Support@samriddhkendra.in",
+      mobile: "9153985960",
+    },
+    grievance: {
+      email: "grievance.samriddh@gmail.com",
+      mobile: "9153985959",
+    },
   };
 
   return (
-    <div className="container GrievanceOfficer mt-15 font-inter fs-20">
-      <h1 className="text-center mb-5 fw-bold">
+    <div className="container GrievanceOfficer mt-15 font-inter fs-20 px-5">
+      <h1 className="text-center mb-4 fw-bold">
         Grievance Officer Contact Information
       </h1>
-      <table className="table table-bordered table-striped">
-        <thead className="thead-dark">
-          <tr>
-            <th>Field</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Email ID</td>
-            <td>{contactInfo.email}</td>
-          </tr>
-          <tr>
-            <td>Mobile Number</td>
-            <td>{contactInfo.mobile}</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="table-responsive hideScrollBar">
+        <table className="table table-bordered table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th>Contact Type</th>
+              <th>Email ID</th>
+              <th>Mobile Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Merchant Support</td>
+              <td>{contactInfo.merchantsupport.email}</td>
+              <td>{contactInfo.merchantsupport.mobile}</td>
+            </tr>
+            <tr>
+              <td>Support</td>
+              <td>{contactInfo.support.email}</td>
+              <td>{contactInfo.support.mobile}</td>
+            </tr>
+            <tr>
+              <td>Grievance</td>
+              <td>{contactInfo.grievance.email}</td>
+              <td>{contactInfo.grievance.mobile}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
